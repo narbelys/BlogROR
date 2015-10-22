79932d4ad4692107ba8ed89b9fea381fe4796e75
narbelys <narbelys@destacame.cl>
2015-10-22 16:40:57 -0300

    agregar comentarios

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,57 @@
 class CommentsController < ApplicationController
+    
+  def index
+    @comment = Article.all
+  end
+    
+  def new
+    @comment = Article.comments.new
+  end
+
+  def create
+      
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.new(comment_params)
+    
+    print @comment.save
+    print @comment.errors.full_messages
+      
+    if @comment.save
+        flash[:success] = "Comentario enviado"
+      redirect_to article_path(@article)
+    else
+        flash[:error] = @comment.errors.full_messages
+        redirect_to article_path(@article)
+       #render :json => ['hotels' => @article.comments, 'country' =>  @article]
+    end
+    
+      
+    #@article = Article.find(params[:article_id])
+    #@comment = @article.comments.new(comment_params)
+    #redirect_to article_path(@article)
+      
+    #if @comment.save
+        #redirect_to article_path(@article)
+         #redirect_to @comment
+    #else
+        #print @comment.errors.full_messages
+        #print '----------'
+        #redirect_to article_path(@article)
+        #render 'articles/show'
+    #end
+      
+      
+  end
+    
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+    
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
+  validates :commenter, presence: true,
+                length: { minimum: 3 }  
+    
+  validates :body, presence: true,
+                length: { minimum: 5 }
+    
 end
